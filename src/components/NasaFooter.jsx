f51b4259d050211b25b114aafa2bb0959dfd4369
Prince Kumar Sharma <ipreencekmr@gmail.com>
2023-06-27 22:17:29 +0530

    update module

--- src/components/NasaFooter.jsx
+++ src/components/NasaFooter.jsx
@@ -4,15 +4,46 @@ import { loadLanguagePack, updateLocale } from '@americanexpress/one-app-ducks';
 import { IntlProvider } from 'react-intl';
 import { connect } from 'react-redux';
 import { fromJS } from 'immutable';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      .
+    </Typography>
+  );
+}
 
 export const NasaFooter = ({ languageData, localeName }) => {
-  // Read about loading async data:
-  // https://github.com/americanexpress/one-app/blob/main/docs/api/modules/Loading-Data.md
-  // quick and dirty solution - implement based on your use case
   if (languageData) {
     return (
       <IntlProvider locale={localeName} messages={languageData}>
-        Footer
+        <Box
+          component="footer"
+          sx={{
+            py: 3,
+            px: 2,
+            mt: 'auto',
+            backgroundColor: (theme) => (theme.palette.mode === 'light'
+              ? theme.palette.grey[200]
+              : theme.palette.grey[800]),
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography variant="body1">
+              My sticky footer can be found here.
+            </Typography>
+            <Copyright />
+          </Container>
+        </Box>
       </IntlProvider>
     );
   }
